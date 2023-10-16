454eca4f9e9c8e88c9a0a7a4da638aa314004a21
diegoalrv <d.alexis.ramirez@gmail.com>
2023-10-16 11:38:15 -0300

    added script to capture picture using opencv

---
+++ capture.py
@@ -0,0 +1,56 @@
+import cv2
+import os
+import datetime
+
+def generate_timestamp_filename(extension=".jpg"):
+    # Obtiene la fecha y hora actual
+    current_time = datetime.datetime.now()
+
+    # Formatea la fecha y hora como una cadena legible
+    timestamp = current_time.strftime("%Y%m%d%H%M%S")
+
+    # Combina la marca de tiempo con la extensión del archivo
+    filename = timestamp + extension
+
+    return filename
+
+def get_capture(num_camera=0):
+
+    # Inicializa la cámara
+    cap = cv2.VideoCapture(num_camera)  # El número '0' indica la cámara predeterminada (puede ser otra cifra si tienes varias cámaras)
+
+    if not cap.isOpened():
+        print("Error: No se puede abrir la cámara.")
+        exit()
+
+    # Captura un fotograma de la cámara
+    ret, frame = cap.read()
+
+    # Libera la cámara
+    cap.release()
+
+    # Cierra todas las ventanas de OpenCV
+    cv2.destroyAllWindows()
+
+    if not ret:
+        print("Error: No se pudo capturar la imagen.")
+        exit()
+    else:
+        return frame
+
+
+# Ejemplo de uso:
+if __name__ == "__main__":
+    save_path = os.path.join('data', 'output')
+
+    filename = generate_timestamp_filename()
+    filename = os.path.join(save_path, filename)
+
+    frame = get_capture()
+    try:
+        # Guarda la imagen capturada en un archivo
+        cv2.imwrite(filename, frame)
+
+        print(f"Imagen capturada y guardada como '{filename}'")
+    except:
+        print("Falló en la captura")
