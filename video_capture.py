ee960daee6b9831c32b3926a9b22ff7b88f02874
diegoalrv <d.alexis.ramirez@gmail.com>
2023-10-17 15:48:13 -0300

    modified video_capture script to activate mode without recording

--- video_capture.py
+++ video_capture.py
@@ -1,7 +1,8 @@
 import cv2
 import os
+import argparse
 
-def get_video_capture(num_camera=0, output_path='data/output/video.mp4'):
+def get_video_capture(num_camera=0, output_path='data/output/video.mp4', recording=True):
     # Inicializa la cámara
     cap = cv2.VideoCapture(num_camera)  # El número '0' indica la cámara predeterminada
 
@@ -12,6 +13,8 @@ def get_video_capture(num_camera=0, output_path='data/output/video.mp4'):
     # Define el codificador para el archivo de salida
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_path, fourcc, 20.0, (640, 480))  # Puedes ajustar la resolución y la velocidad de fotogramas
+
+    recording = True  # Inicialmente, estamos grabando
 
     while True:
         # Captura un fotograma de la cámara
@@ -24,21 +27,32 @@ def get_video_capture(num_camera=0, output_path='data/output/video.mp4'):
         # Muestra el fotograma en una ventana
         cv2.imshow('Video', frame)
 
-        # # Guarda el fotograma en el archivo de video
-        # out.write(frame)
+        # Guarda el fotograma en el archivo de video si estamos grabando
+        if recording:
+            out.write(frame)
 
         # Presiona 'q' para salir del bucle
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
             break
+        elif key == ord('r'):  # Presiona 'r' para alternar la grabación
+            recording = not recording
 
     # Libera la cámara y cierra la ventana
     cap.release()
     out.release()
     cv2.destroyAllWindows()
 
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(description='Captura de video desde una cámara.')
+    parser.add_argument('--recording', type=int, default=1, choices=[0, 1],
+                        help='Activa o desactiva la grabación de video (0 para desactivar, 1 para activar).')
+    args = parser.parse_args()
+
+    recording = bool(args.recording)
+
     save_path = os.path.join('data', 'output')
-    
+
     # Verifica si la carpeta 'data' existe, si no, la crea
     if not os.path.exists('data'):
         os.makedirs('data')
@@ -47,4 +61,7 @@ if __name__ == "__main__":
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
-    get_video_capture()
+    get_video_capture(recording=recording)
+
+if __name__ == "__main__":
+    main()
